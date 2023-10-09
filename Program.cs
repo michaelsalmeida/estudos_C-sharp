be5192b1691bb3c681e8ca7cb5695bcb0b8b4a5d
michaelsalmeida <112515310+michaelsalmeida@users.noreply.github.com>
2023-10-09 13:10:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Screen Sound
+
+
+void ExibirMensagemDeBoasVindas() {
+    Console.WriteLine("***********************");
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine("***********************");
+}
+
+
+void OpcoesDoMenu(){
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcao = Console.ReadLine()!;
+    int escolha = int.Parse(opcao);
+
+    switch (escolha) {
+        case 1 : Console.WriteLine("Voce escolheu a opção " + escolha); 
+            break;
+        case 2 : Console.WriteLine("Voce escolheu a opção " + escolha); 
+            break;
+        case 3 : Console.WriteLine("Voce escolheu a opção " + escolha); 
+            break;
+        case 4 : Console.WriteLine("Voce escolheu a opção " + escolha); 
+            break;
+        case -1 : Console.WriteLine("Voce escolheu a opção " + escolha); 
+            break;
+
+        default: Console.WriteLine("Digite uma opção válida");
+            break;
+    }
+
+}
+
+ExibirMensagemDeBoasVindas();  
+OpcoesDoMenu();
